ac4ae89dc51f407b9c9e3a347f68123d2e6b16cf
i-figueroa <ingelbert.figueroa@gateb.com>
2022-07-12 14:59:31 -0700

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // server: {
-  //   port: 3000
-  // }
+  server: {
+    port: process.env.PORT || 3000
+  }
 })

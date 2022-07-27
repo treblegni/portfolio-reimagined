90f4e6787bd5980a79d6a15032095bbc1c537754
i-figueroa <=>
2022-07-27 11:41:06 -0700

    updated api

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -8,27 +8,35 @@
       <SVGRenderer
         :svg="eyeSVGString"
         class="social-icon h-6 w-auto fill-current text-cream"></SVGRenderer>
-      <span class="tab">Watch Count</span>
+      <span class="tab" v-text="watchCount"></span>
     </div>
     <div class="flex items-center">
       <SVGRenderer
         :svg="starSVGString"
         class="social-icon h-6 w-auto fill-current text-cream"></SVGRenderer>
-      <span class="tab">Star count</span>
+      <span class="tab" v-text="starCount"></span>
     </div>
   </div>
 </div>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
+import { ref,computed,onMounted,Ref } from 'vue'
 import { navigationStore } from '../stores/navigation-store'
+import { getRepo } from '../utils/helper-functions'
 import SVGRenderer from './SVGRenderer.vue'
 import eyeSVGString from '../assets/iconmonstr-eye-lined.svg?raw'
 import starSVGString from '../assets/iconmonstr-star-lined.svg?raw'
+
+const watchCount:Ref<Number> = ref<Number>(0)
+const starCount:Ref<Number> = ref<Number>(0)
 
 const footer = computed(() => ({
   'text-center': navigationStore.isMobile,
   'tab': true
 }))
+
+onMounted(() => {
+  console.log(getRepo())
+})
 </script>
--- src/utils/helper-functions.ts
+++ src/utils/helper-functions.ts
@@ -1,4 +1,9 @@
+import axios from 'axios';
 import { navigationStore } from '../stores/navigation-store';
+
+const endpoint = '/api/get-repo'
+const user = 'treblegni'
+const repo = 'portfolio-reimagined'
 
 export const mobileStateHandler = () : void => {
   const size = 1024
@@ -16,4 +21,17 @@ export const mobileStateHandler = () : void => {
       navigationStore.updateMobile(false)
     }
   })
+}
+
+export const getRepo = () : Number => {
+  axios({
+    method: 'GET',
+    url: `${endpoint}?user=${user}&repo=${repo}`
+  })
+  .then(res => res.data)
+  .then(data => {
+    return data
+  })
+  .catch(err => console.log(err))
+  return 0
 }
